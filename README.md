22b4e6ba2100c2cc8f653972ec06747aac1ce479
Vhin <100606172+coder060703@users.noreply.github.com>
2023-11-18 14:39:59 +0800

    FACERECOGPART

---
+++ README.md
@@ -0,0 +1,1 @@
+"# ATFLCASESTUDY" 
---
+++ main.py
@@ -0,0 +1,62 @@
+import cvzone
+import os
+import pickle
+import numpy as np
+import face_recognition
+import cv2
+
+cap = cv2.VideoCapture(0)
+cap.set(3, 640)
+cap.set(4, 480)
+
+imgBackground = cv2.imread('Resources/background.png')
+
+#importing MODES IMAGES IN LIST
+folderModePath = 'Resources/Modes'
+modePathList = os.listdir(folderModePath)
+imgModeList = []
+for path in modePathList:
+    imgModeList.append(cv2.imread(os.path.join(folderModePath, path)))
+# print(len(imgModeList))
+
+#load the encoding file
+print("Loading Student Datas...")
+file = open("EncodeFile.p", 'rb')
+encodeListKnownWithIds = pickle.load(file)
+file.close
+encodeListKnown,studentIds = encodeListKnownWithIds
+#print(studentIds)  #just for checking the list 
+print("Student Datas Loaded")
+
+while True:
+    success, img = cap.read()
+
+    imgS = cv2.resize(img, (0,0), None, 0.25, 0.25)
+    imgS = cv2.cvtColor(imgS,cv2.COLOR_BGR2RGB)
+
+    faceCurFrame = face_recognition.face_locations(imgS)
+    encodeCurFrame = face_recognition.face_encodings(imgS,faceCurFrame)
+
+    imgBackground[162:162+480,55:55+640] = img
+    imgBackground[44:44+633, 808:808 + 414] = imgModeList[1]
+
+    for encodeFace, faceLoc in zip (encodeCurFrame, faceCurFrame):
+        matches = face_recognition.compare_faces(encodeListKnown, encodeFace)
+        faceDis = face_recognition.face_distance(encodeListKnown, encodeFace)
+        # print("Matches", matches)
+        # print("faceDis", faceDis)
+
+        matchIndex = np.argmin(faceDis)
+        # print("Match Index", matchIndex)
+
+        if matches[matchIndex]:
+           # print("Known Face Detected")
+           # print(studentIds[matchIndex])
+            y1,x2,y2,x1 = faceLoc
+            y1, x2, y2, x1 = y1 * 4, x2 * 4, y2 * 4, x1 * 4
+            bbox = 55 + x1, 162 + y1, x2 - x1, y2 - y1
+            imgBackground = cvzone.cornerRect(imgBackground,bbox,rt=0)
+
+    # cv2.imshow("Scan Face", img)
+    cv2.imshow("Face Recognition", imgBackground)
+    cv2.waitKey(1)
